495417adb6e2de3283f29ea2b9f7c4487a3ce13c
AquaApple <yassershehab9@gmail.com>
2022-02-11 17:20:23 +0200

    Added Login & Register

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,14 +1,37 @@
+const User = require('../models/User')
+
 
 const getAllUsers =  async (req,res)=>{
+  
    res.send("All users")
 }
 const register=  async (req,res)=>{
-    res.send("Register")
+  try{
+   const {name,email,password}=req.body; 
+   const user = await User.create({
+      name,
+      email,
+      password
+   })
+    res.status(201).send(user);
+  }catch(err){
+   res.status(400).send(err.message);
+  }
  }
 
  
 const login=  async (req,res)=>{
-    res.send("you are login")
+   try{ 
+      const {email,password}=req.body;
+      const user = await User.findOne({email})
+      if(!user){
+         res.status(404).send("User Not Found");
+      }
+      res.status(200).send(user);
+   }catch(err){
+      res.status(400).send(err.message)
+   }
+  
  }
 
  const profile=  async (req,res)=>{
