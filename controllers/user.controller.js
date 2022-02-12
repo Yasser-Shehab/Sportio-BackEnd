ff8c012958c6970f63cf59480c826e33b8e13ec3
HishamAfifi <hish.abdelshafouk@gmail.com>
2022-02-12 00:40:47 -1000

    Added Authentication.

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,41 +1,55 @@
-const User = require('../models/User')
+const User = require("../models/User");
+const JWT = require("jsonwebtoken");
 
-
-const getAllUsers =  async (req,res)=>{
-  
-   res.send("All users")
-}
-const register=  async (req,res)=>{
-  try{
-   const {name,email,password}=req.body; 
-   const user = await User.create({
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.status(200).send(users);
+  } catch (err) {
+    res.status(404).send(err.message);
+  }
+};
+const register = async (req, res) => {
+  try {
+    const isFirst = (await User.countDocuments({})) === 0;
+    const role = isFirst ? "admin" : "user";
+    const { name, email, password } = req.body;
+    const user = await User.create({
       name,
       email,
-      password
-   })
+      password,
+      role,
+    });
     res.status(201).send(user);
-  }catch(err){
-   res.status(400).send(err.message);
+  } catch (err) {
+    res.status(400).send(err.message);
   }
- }
+};
 
- 
-const login=  async (req,res)=>{
-   try{ 
-      const {email,password}=req.body;
-      const user = await User.findOne({email})
-      if(!user){
-         res.status(404).send("User Not Found");
-      }
-      res.status(200).send(user);
-   }catch(err){
-      res.status(400).send(err.message)
-   }
-  
- }
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).send("User Not Found");
+    }
+    const matched = await user.comparePassword(password);
 
- const profile=  async (req,res)=>{
-    res.send("User data")
- }
+    if (!matched) {
+      return res.status(401).send("Invalid email or password");
+    }
+    const userToken = JWT.sign(
+      { _id: user._id, email: user.email },
+      process.env.JWT_SECRET
+    );
+    res.status(200).send(userToken);
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+};
 
-module.exports={register,login,profile,getAllUsers}
+const profile = async (req, res) => {
+  res.send("User data");
+};
+
+module.exports = { register, login, profile, getAllUsers };
--- models/User.js
+++ models/User.js
@@ -1,30 +1,39 @@
-const mongoose= require("mongoose");
-const validators= require("validator")
+const mongoose = require("mongoose");
+const validators = require("validator");
+const bcrypt = require("bcryptjs");
 
-const userSchema=new mongoose.Schema({
-    name:{
-        type:String,
-        required:[true,"Name is required"]
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    validate: {
+      validator: validators.isEmail,
+      massage: "Invalid email",
     },
-    email:{
-        type:String,
-        required:[true,"Email is required"],
-        validate:{
-            validator: validators.isEmail,
-            massage:"Invalid email"
-        },
-        unique:true
-    },
-    password:{
-        type:String,
-        required:[true,"Password is required"],
-        minlength:8
-    },
-    role:{
-        type:String,
-        enum:['admin','user'],
-        default:"user"
-    }
-})
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+    minlength: 8,
+  },
+  role: {
+    type: String,
+    enum: ["admin", "user"],
+    default: "user",
+  },
+});
 
-module.exports=mongoose.model("User",userSchema)
+userSchema.pre("save", async function () {
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+userSchema.methods.comparePassword = async function (pass) {
+  return await bcrypt.compare(pass, this.password);
+};
+
+module.exports = mongoose.model("User", userSchema);
