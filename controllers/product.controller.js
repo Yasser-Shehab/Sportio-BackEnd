5baf0e19d1ea89f7c265349112b159294bc37ca6
HishamAfifi <hish.abdelshafouk@gmail.com>
2022-02-18 19:11:22 -1000

    change imagePath

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -64,7 +64,9 @@ const uploadImage = async (req, res) => {
     const imagePath = path.join(__dirname, `../public/uploads/${name}`);
     await req.files.image.mv(imagePath);
     console.log(req.files.image);
-    res.status(200).send({ imagePath });
+    res.status(200).send({
+      imagePath: `https://sportio-backend.herokuapp.com/uploads/${name}`,
+    });
   } catch (err) {
     res.status(400).send({ message: err.message });
   }
